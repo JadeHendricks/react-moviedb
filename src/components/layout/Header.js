66faf544730ae82ef40952bef4b9edf8a7c69144
jade <hendricksjade@gmail.com>
2020-03-18 19:47:19 +0200

    setting genres on the header component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import svgAssets from "../../images/sprite.svg";
 import { Link } from "react-router-dom";
 import moment from "moment";
 
-const Header = ({ movie: { backdrop_path, title, overview, release_date, id } }) => {
+const Header = ({ movie: { backdrop_path, title, overview, release_date, id, genre_ids } }) => {
+
+  useEffect(() => {
+    setGenresArray(genre_ids);
+  }, [genre_ids]);
+
+  const [genres, setGenres] = useState([]);
 
   const backgroundImage = {
     backgroundImage: backdrop_path ? `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://image.tmdb.org/t/p/original${backdrop_path})` : "",
@@ -23,21 +29,33 @@ const Header = ({ movie: { backdrop_path, title, overview, release_date, id } })
     );
   }
 
+
+  const setGenresArray = async (genre_ids) => {
+
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+    const data = await response.json();
+    if (genre_ids) {
+      const genresArray = data.genres.filter(element => genre_ids.includes(element.id));
+      setGenres(genresArray);
+    }
+  }
+
   return (
     <header className="movie-header" style={backgroundImage}>
       <div className="container-small">
           <svg className="movie-header__playicon" onClick={playTrailer}>
-              <use xlinkHref={`${svgAssets}#icon-play2`}></use>
+              <use xlinkHref={`${svgAssets}#icon-play2`}>
+                <title>Play {title} trailer</title>
+              </use>
           </svg>
           <h4 className="movie-header__slug">Most Popular</h4>
           <div className="category">
               <div className="category__wrapper">
-                  <div className="category__pill category__pill--large category__pill--white">Drama</div>
-                  <div className="category__pill category__pill--large category__pill--white">Sci-fi</div>
+                {genres.slice(0, 3).map(genre => <div key={genre.id} className="category__pill category__pill--large category__pill--white">{genre.name}</div>)}
               </div>
           </div>
           <h1 className="movie-header__title">{ title }</h1>
-          <span className="movie-header__date">{ moment(release_date).format("DD MMM YYYY") } &nbsp; | &nbsp; 240 mins.&nbsp;</span>
+          <span className="movie-header__date">{ moment(release_date).format("DD MMM YYYY") }</span>
           <p className="movie-header__description">
             { overview }
           </p>
--- src/components/layout/SideNavigation.js
+++ src/components/layout/SideNavigation.js
@@ -3,34 +3,32 @@ import svgAssets from "../../images/sprite.svg";
 import { Link } from "react-router-dom";
 
 function SideNavigation() {
-    
-  const noTextDecoration = {textDecoration: "none"}
 
   return (
     <nav className="navigation">
-        <Link to="/" style={{textDecoration: "none"}}>
+        <Link to="/" style={{ textDecoration: "none" }}>
             <h3 className="navigation__logo">MDBv2</h3>
         </Link>
         <div className="navigation__wrapper">
-            <a className="navigation__node active" href="/" style={noTextDecoration}>
+            <a className="navigation__node active" href="/">
                 <svg className="navigation__icon">
                     <use xlinkHref={`${svgAssets}#icon-video`}></use>
                 </svg>
                 <h5 className="navigation__nodename">Now Playing</h5>
             </a>
-            <a className="navigation__node" href="/" style={noTextDecoration}>
+            <a className="navigation__node" href="/">
                 <svg className="navigation__icon">
                     <use xlinkHref={`${svgAssets}#icon-desktop`}></use>
                 </svg>
                 <h5 className="navigation__nodename">Top Rated</h5>
             </a>
-            <a className="navigation__node" href="/" style={noTextDecoration}>
+            <a className="navigation__node" href="/">
                 <svg className="navigation__icon">
                     <use xlinkHref={`${svgAssets}#icon-presentation`}></use>
                 </svg>
                 <h5 className="navigation__nodename">Popular</h5>
             </a>
-            <a className="navigation__node" href="/" style={noTextDecoration}>
+            <a className="navigation__node" href="/">
                 <svg className="navigation__icon">
 
                     <use xlinkHref={`${svgAssets}#icon-clock`}></use>
