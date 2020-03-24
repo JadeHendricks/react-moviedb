2d41838e6962394956a0ead3bba3b08a04940dea
jade <hendricksjade@gmail.com>
2020-03-24 15:55:37 +0200

    added series to the searchresults

--- src/components/informational/SearchResults.js
+++ src/components/informational/SearchResults.js
@@ -6,18 +6,28 @@ function SearchResults() {
   const query = decodeURI(window.location.href.split("/").pop());
 
   useEffect(() => {
-    fetchSearch(query);
+    fetchMoviesAndSeries(query);
     window.scrollTo(0, 0);
     // eslint-disable-next-line
   }, [query]);
 
+
   const [searchResults, setSearchResults] = useState([]);
 
-  const fetchSearch = async (query) => {
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`);
-    const data = await response.json();
-    data && setSearchResults(data.results);
+  const fetchMoviesAndSeries = async (query) => {
+    const responseMovies = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`);
+    const responseSeries = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`);
+
+    const dataMovies = await responseMovies.json();
+    const dataSeries = await responseSeries.json();
+
+    const resultsArray = [...dataMovies.results, ...dataSeries.results];
+    const mostPopluarOrder =  resultsArray.sort((a, b) => b.popularity - a.popularity);
+
+    setSearchResults(mostPopluarOrder);
   }
+
+
 
   return (
     <section className="searchResults">
